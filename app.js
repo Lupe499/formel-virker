ec0f1ce404ef2283049bf79ccf65fc2c919c9c47
Lucas Pedersen <lucas.a.pedersen@gmail.com>
2020-08-26 12:25:04 +0200

    done

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import formValidator from "./form-validator.js";
+import quotinator from "./quotinator.js";
+
 
 formValidator(document.querySelector("#form"));
+quotinator();
 //formValidator(document.querySelector("something else"));
---
+++ quotinator.js
@@ -0,0 +1,9 @@
+function quorinator() {
+    fetch("https://quotes.rest/qod")
+    .then(res => res.json())
+    .then(function (data) {
+        let quote = document.querySelector(".quote");
+        quote.innerText = data.contents.quotes[0].quote;
+    });
+}
+export default quorinator;
